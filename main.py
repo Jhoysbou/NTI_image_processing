01e8c2b7ae41b4e9e64133f5f65b917012632949
Jhoysbou <bakuta.ad@gmail.com>
2020-03-21 19:53:47 +0300

    object detection is done

--- main.py
+++ main.py
@@ -3,17 +3,27 @@ import imutils
 import numpy as np
 from imutils.video import VideoStream
 import time
-# vs = cv2.VideoCapture("./camera/bucket.mov")
 
+# vs = cv2.VideoCapture("./camera/bucket.mov")
 
 vs = VideoStream(src=0).start()
 time.sleep(0.5)
+POOL_LENGTH = 5
 MIN = 200
+DELTA = 1.3
 
-h_min = np.array((158, 195, 0), np.uint8)
-h_max = np.array((255, 255, 255), np.uint8)
 
-DELTA = 7
+def circle_check(pool, coords):
+    if pool:
+        for circles in pool:
+            for circle in circles:
+                if max(abs(np.array(circle[:-1]) - np.array(coords))) < DELTA * circle[2]:
+                    return False
+
+    return True
+
+
+circles_coords_pool = list()
 
 while True:
     frame = vs.read()
@@ -25,14 +35,16 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     hsv = cv2.GaussianBlur(hsv, (19, 19), 0)
     gray = cv2.cvtColor(hsv, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 127, 255, 1)
+    ret, thresh = cv2.threshold(gray, 100, 255, 1)
     thresh = cv2.bitwise_not(thresh)
     cv2.imshow("thresh", thresh)
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = []
-    circles_coords = []
+
+    if circles_coords_pool is not None and len(circles_coords_pool) >= POOL_LENGTH:
+        circles_coords_pool.pop()
 
     circles = cv2.HoughCircles(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), cv2.HOUGH_GRADIENT, 1.5, 20)
     if circles is not None:
@@ -43,7 +55,9 @@ while True:
             # draw the circle in the output image, then draw a rectangle
             # corresponding to the center of the circle
             cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-        circles_coords.append([x, y])
+            circles = list()
+            circles.append([x, y, r])
+        circles_coords_pool.append(circles)
 
     for c in cnts:
         # if the contour is too small, ignore it
@@ -51,14 +65,11 @@ while True:
             continue
 
         (x, y, w, h) = cv2.boundingRect(c)
-        if not circles_coords:
+        if circle_check(circles_coords_pool, [x, y]):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             center.append([int(x + w / 2), int(y + h / 2)])
-        for circle in circles_coords:
-            if max((np.array([x, y]) - np.array(circle))) > DELTA:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                center.append([int(x + w/2), int(y + h/2)])
-        print(center)
+        # print(center)
+
     cv2.imshow("result", frame)
     key = cv2.waitKey(1) & 0xFF
 
