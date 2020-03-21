76dd7f9a240c992495af912384e3c14bf209ca51
Jhoysbou <bakuta.ad@gmail.com>
2020-03-21 18:45:33 +0300

    object detection with center

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import cv2
 import imutils
 import numpy as np
+from imutils.video import VideoStream
+import time
+# vs = cv2.VideoCapture("./camera/bucket.mov")
 
-vs = cv2.VideoCapture("./camera/bucket.mov")
+
+vs = VideoStream(src=0).start()
+time.sleep(0.5)
 MIN = 200
 
 h_min = np.array((158, 195, 0), np.uint8)
 h_max = np.array((255, 255, 255), np.uint8)
 
+DELTA = 7
+
 while True:
-    frame = vs.read()[1]
+    frame = vs.read()
     frame = imutils.resize(frame, width=640, height=360)
 
     if frame is None:
         break
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    blur = cv2.GaussianBlur(hsv, (19, 19), 0)
-
-    thresh = cv2.inRange(blur, h_min, h_max)
+    hsv = cv2.GaussianBlur(hsv, (19, 19), 0)
+    gray = cv2.cvtColor(hsv, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, 1)
+    thresh = cv2.bitwise_not(thresh)
     cv2.imshow("thresh", thresh)
-
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
     center = []
+    circles_coords = []
 
     circles = cv2.HoughCircles(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), cv2.HOUGH_GRADIENT, 1.5, 20)
     if circles is not None:
@@ -35,7 +43,7 @@ while True:
             # draw the circle in the output image, then draw a rectangle
             # corresponding to the center of the circle
             cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
-
+        circles_coords.append([x, y])
 
     for c in cnts:
         # if the contour is too small, ignore it
@@ -43,9 +51,14 @@ while True:
             continue
 
         (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        center.append([int(x + w/2), int(y + h/2)])
-
+        if not circles_coords:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            center.append([int(x + w / 2), int(y + h / 2)])
+        for circle in circles_coords:
+            if max((np.array([x, y]) - np.array(circle))) > DELTA:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                center.append([int(x + w/2), int(y + h/2)])
+        print(center)
     cv2.imshow("result", frame)
     key = cv2.waitKey(1) & 0xFF
 
