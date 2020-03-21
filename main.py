d47161e711fae0f897d170997e7496015f5e5493
Jhoysbou <bakuta.ad@gmail.com>
2020-03-21 14:59:01 +0300

    open cv scratch

--- main.py
+++ main.py
@@ -1,9 +1,41 @@
-from wolframclient.evaluation import WolframLanguageSession
-from wolframclient.language import wl
+import cv2
+import imutils
+import numpy as np
 
+vs = cv2.VideoCapture("./camera/WIN_20200321_13_34_37_Pro.mp4")
+MIN = 100
+h_min = np.array((0, 0, 0), np.uint8)
+h_max = np.array((255, 216, 255), np.uint8)
 
-kernel = '/Applications/Wolfram Engine.app/Contents/MacOS/WolframKernel'
-session = WolframLanguageSession(kernel=kernel)
+while True:
+    frame = vs.read()[1]
+    frame = imutils.resize(frame, width=640, height=360)
+    blur = cv2.GaussianBlur(frame, (19, 19), 0)
 
-sample = session.evaluate(wl.RandomVariate(wl.NormalDistribution(0,1), 1e6))
-print(sample)
+    thresh = cv2.inRange(blur, h_min, h_max)
+    cv2.imshow("thresh", thresh)
+
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    if frame is None:
+        break
+
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < MIN:
+            continue
+
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("result", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+vs.release()
+cv2.destroyAllWindows()
